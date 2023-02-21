f0fe65d4cd79d7272930f232b05a85af37e11c5d
xing-6464 <xing_6464@foxmail.com>
2023-02-21 19:44:36 +0800

    反转链表

--- js-code/src/01-algorithm/reverse-link-list.ts
+++ js-code/src/01-algorithm/reverse-link-list.ts
@@ -3,12 +3,52 @@
  * @author  星光
  */
 
-interface ILinkListNode {
+export interface ILinkListNode {
     value: number
     next?: ILinkListNode
 }
 
-function createLinkList(arr: number[]): ILinkListNode {
+/**
+ * 反转单向链表，并返回反转之后的 head node
+ * @param listNode ILinkListNode
+ */
+export function reverseLinkList(listNode: ILinkListNode): ILinkListNode {
+    if (listNode.next == undefined) return listNode
+
+    // 定义三个指针
+    let prenNode: ILinkListNode | undefined = undefined
+    let curNode: ILinkListNode | undefined = undefined
+    let nextNode: ILinkListNode | undefined = listNode
+
+    // 以nextNode为主，遍历链表
+    while (nextNode) {
+        // 第一个元素，删掉 next，防止循环引用
+        if (curNode && !prenNode) {
+            delete curNode.next
+        }
+
+        // 反转指针
+        if (curNode && prenNode) {
+            curNode.next = prenNode
+        }
+
+        // 整体指针移动
+        prenNode = curNode
+        curNode = nextNode
+        nextNode = nextNode?.next
+    }
+
+    curNode!.next = prenNode
+
+    return curNode!
+}
+
+/**
+ * 根据数组创建单向链表
+ * @param arr number[]
+ * @returns ILinkListNode
+ */
+export function createLinkList(arr: number[]): ILinkListNode {
     const length = arr.length
     if (length === 0) throw new Error('arr is empty')
 
@@ -28,5 +68,9 @@ function createLinkList(arr: number[]): ILinkListNode {
     return curNode
 }
 
-const arr = [100, 200, 300, 400, 500]
-console.log(createLinkList(arr))
+// const arr = [100, 200, 300, 400, 500]
+// let list = createLinkList(arr)
+// console.info('list', list)
+
+// const list1 = reverseLinkList(list)
+// console.info('list1', list1)
